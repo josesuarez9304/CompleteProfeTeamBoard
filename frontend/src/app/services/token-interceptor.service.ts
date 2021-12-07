02b37b8213d0bc46a0fb00efeddb085be8a9421e
josesuarez9304 <josesuarezman1@gmail.com>
2021-12-07 10:12:31 -0500

    Firts commit

---
+++ frontend/src/app/services/token-interceptor.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { UserService } from './user.service';
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TokenInterceptorService implements HttpInterceptor {
+  constructor(private _userService: UserService) {}
+  intercept(req: any, next: any) {
+    const tokenReq = req.clone({
+      setHeaders: {
+        Authorization: 'Bearer ' + this._userService.getToken(),
+      },
+    });
+    return next.handle(tokenReq);
+  }
+}
